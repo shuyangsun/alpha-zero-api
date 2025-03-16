20daaca0e06322f1a168bf26e9902da2b0e64d6b
Shuyang Sun <shuyangsun10@gmail.com>
2025-03-16 11:45:01 -0400

    Add PolicyNetwork class.

---
+++ api/policy_output.cc
@@ -0,0 +1,24 @@
+#include "api/policy_output.h"
+
+#include <iterator>
+#include <utility>
+#include <vector>
+
+namespace az::interface {
+
+PolicyOutput::PolicyOutput(float value, std::vector<float>&& probabilities)
+    : value{value}, probabilities{std::move(probabilities)} {}
+
+PolicyOutput::PolicyOutput(float value, const std::vector<float>& probabilities)
+    : value{value}, probabilities{probabilities} {}
+
+PolicyOutput::PolicyOutput(std::vector<float>&& nn_output)
+    : value{nn_output.front()},
+      probabilities{std::make_move_iterator(nn_output.begin() + 1),
+                    std::make_move_iterator(nn_output.end())} {}
+
+PolicyOutput::PolicyOutput(const std::vector<float>& nn_output)
+    : value{nn_output.front()},
+      probabilities{nn_output.begin() + 1, nn_output.end()} {}
+
+}  // namespace az::interface
---
+++ api/policy_output.h
@@ -0,0 +1,74 @@
+#ifndef ALPHA_ZERO_API_POLICY_OUTPUT_H_
+#define ALPHA_ZERO_API_POLICY_OUTPUT_H_
+
+#include <vector>
+
+namespace az::interface {
+
+/**
+ * @brief ** DO NOT MODIFY ** PolicyOutput class is the output of the policy
+ * neural network.
+ *
+ * It contains the value of the current state and the vector of
+ * probabilities of selecting each action. The value and probabilities are
+ * single-precision floating point values.
+ */
+struct PolicyOutput {
+  PolicyOutput() = delete;
+
+  /**
+   * @brief Construct a new PolicyOutput object from a value and an r-value
+   * vector of probabilities.
+   *
+   * @param value Value of the current state.
+   * @param probabilities Vector of probabilities of selecting each action.
+   */
+  PolicyOutput(float value, std::vector<float>&& probabilities);
+
+  /**
+   * @brief Construct a new PolicyOutput object from a value and an l-value
+   * vector of probabilities.
+   *
+   * @param value Value of the current state.
+   * @param probabilities Vector of probabilities of selecting each action.
+   */
+  PolicyOutput(float value, const std::vector<float>& probabilities);
+
+  /**
+   * @brief Construct a new PolicyOutput object from an r-value vector that
+   * represents the output of the neural network. The first element of the
+   * vector is the value of the current state, and the rest of the elements are
+   * probabilities of selecting each action.
+   *
+   * @param nn_output Vector of single-precision floats that represents the
+   * output of the neural network.
+   */
+  PolicyOutput(std::vector<float>&& nn_output);
+
+  /**
+   * @brief Construct a new PolicyOutput object from an r-value vector that
+   * represents the output of the neural network. The first element of the
+   * vector is the value of the current state, and the rest of the elements are
+   * probabilities of selecting each action.
+   *
+   * @param nn_output Vector of single-precision floats that represents the
+   * output of the neural network.
+   */
+  PolicyOutput(const std::vector<float>& nn_output);
+
+  ~PolicyOutput() = default;
+
+  /**
+   * @brief Value of the current state.
+   */
+  float value;
+
+  /**
+   * @brief Probablities of selecting each action.
+   */
+  std::vector<float> probabilities;
+};
+
+}  // namespace az::interface
+
+#endif  // ALPHA_ZERO_API_POLICY_OUTPUT_H_
